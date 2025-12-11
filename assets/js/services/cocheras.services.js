db1e57083c009709f5e0bfbd94c8b6ac42b2e709
consalud <compuzettaoficial@gmail.com>
2025-12-11 13:08:25 -0500

    Update cocheras.services.js

--- assets/js/services/cocheras.services.js
+++ assets/js/services/cocheras.services.js
@@ -1,6 +1,6 @@
 class CocherasService {
   static async getCocherasByEvento(eventoId) {
-    const response = await fetch('/data/cocheras.json');
+    const response = await fetch('/hospitalidad_en_accion/data/cocheras.json');
     const data = await response.json();
     return data.cocheras.filter(c => c.eventoId === eventoId)
       .slice(0, 3); // Máximo 3 cocheras
